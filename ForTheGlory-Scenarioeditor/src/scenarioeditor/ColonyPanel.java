aa9d698fb948a3ae08b7fa5933420f0fb8d50bb7
Grunzwanzling <maximilian@gaisberg-helfenberg.de>
2015-09-08 18:15:15 +0200

    idk, idc2

--- ForTheGlory-Scenarioeditor/src/scenarioeditor/ColonyPanel.java
+++ ForTheGlory-Scenarioeditor/src/scenarioeditor/ColonyPanel.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -257,6 +259,42 @@ public class ColonyPanel extends JPanel {
 
 		addComponent(this, layout, chkWhiteman, 1, 15, 2, 1, 0, 0, new Insets(
 				5, 5, 5, 5));
+	}
+
+	public static void main(String[] args) throws IOException {
+		HashMap<String, String> map = new HashMap<String, String>();
+		HashMap<String, String[]> map2 = new HashMap<String, String[]>();
+		String[] climate = { "arctic", "tropical", "temperate", "ncontinental",
+				"scontinental", "tundra", "desertic" };
+		GameFiles game = new GameFiles(
+				"C:\\Program Files (x86)\\Steam\\SteamApps\\common\\For The Glory\\");
+		map2.put("terrain", game.loadTerrains());
+		map2.put("climate", climate);
+		map2.put("religion", game.loadReligions());
+		map2.put("culture", game.loadCultures());
+		map2.put("goods", game.loadGoods());
+
+		map.put("id", "1");
+		map.put("name", "Test");
+		map.put("manpower", "1444555");
+		map.put("income", "245");
+		map.put("value", "0");
+		map.put("cotmodifier", "0");
+		map.put("colonizationdifficulty", "0");
+		map.put("lootedYear", "0");
+		map.put("religion", "hussite");
+		map.put("terrain", "plains");
+		map.put("culture", "abenaki");
+		map.put("climate", "arctic");
+		map.put("goods", "cloth");
+		map.put("looted", "true");
+		map.put("whiteman", "true");
+		JFrame frame = new JFrame();
+		frame.add(new ColonyPanel(map, map2));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+
 	}
 
 	static void addComponent(Container cont, GridBagLayout gbl, Component c,

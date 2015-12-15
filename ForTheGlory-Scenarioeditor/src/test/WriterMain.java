54e5b0f41894d75e390d15d5971e0c6ede4a4a0b
AdmiralJo <admiraljo2013@gmail.com>
2015-12-15 20:32:19 +0100

    Commit

--- ForTheGlory-Scenarioeditor/src/test/WriterMain.java
+++ ForTheGlory-Scenarioeditor/src/test/WriterMain.java
@@ -11,24 +11,21 @@ public class WriterMain {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
 
-		HashMap<String, Object> hash;
-		hash = new HashMap<String, Object>();
+		HashMap<String, Object> hash = new HashMap<String, Object>();
 
-		scenarioeditor.Settings.getSettings("C://Program Files (x86)//Steam//steamapps//common//For The Glory",
-				"German",
-				"1419 - The Grand Campaign.eeg");
-		hash = Settings.hashmap;
+		hash = scenarioeditor.Settings.getSettings("C://Program Files (x86)//Steam//steamapps//common//For The Glory",
+				"German", "1419 - The Grand Campaign.eeg");
+//		hash = Settings.hashmap;
 
 		// System.out.println(((HashMap<String, Object>) ((HashMap<String,
 		// Object>) hash.get("localisationdata")).get("USA")).get("name"));
 
-		// System.out.println(((HashMap<String, Object>) ((HashMap<String,
-		// Object>) hash.get("scenariodata")).get("header")).get("startyear"));
-		// System.out.println("----------------------------");
-		for (String key : ((HashMap<String, Object>) hash.get("countrydata")).keySet()) {
-			System.out.println(key + ((HashMap<String, Object>) ((HashMap<String, Object>) hash.get("countrydata"))));
-
-		}
+		System.out.println(((HashMap<String, Object>) ((HashMap<String, Object>) hash.get("provincedata")).get("10")).get("name"));
+//		System.out.println("----------------------------");
+//		for (String key : ((HashMap<String, Object>) hash.get("provincedata")).keySet()) {
+//			System.out.println(key + ((HashMap<String, Object>) ((HashMap<String, Object>) hash.get("provincedata"))));
+//
+//		}
 
 		// new SaveSettings(hash);
 
